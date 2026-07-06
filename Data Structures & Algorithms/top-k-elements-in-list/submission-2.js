34f224d231f35c4d0d3d4b4f28b3904f3da73cce
denisursu00 <50777176+denisursu00@users.noreply.github.com>
2026-07-07 00:16:39 +0300

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let countMap = new Map();
+        for (let num of nums) {
+            countMap.set(num, (countMap.get(num) || 0) + 1);
+        }
+        let buckets = new Array(nums.length + 1).fill([]);
+        for (let [key, value] of countMap) {
+            buckets[value] = [...buckets[value], key];
+        }
+        let res = [];
+        for (let i = buckets.length - 1; i > 0; i--) {
+            for (let num of buckets[i]) {
+                res.push(num);
+                if (res.length === k) {
+                    return res;
+                }
+            }
+        }
+    }
+}
